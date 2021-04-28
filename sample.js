190a27b4937bd070b9d7d5937111731a47bde369
tegarimansyah <egar.imansyah@gmail.com>
2021-04-28 07:54:21 +0700

    add destroy_instance workflow

--- sample.js
+++ sample.js
@@ -5,6 +5,7 @@ import { Counter } from 'k6/metrics';
 export const requests = new Counter('http_reqs');
 // you can specify stages of your test (ramp up/down patterns) through the options object
 // target is the number of VUs you are aiming for
+
 export const options = {
   stages: [
     { target: 1, duration: '1s' },
@@ -15,6 +16,7 @@ export const options = {
     requests: ['count < 100'],
   },
 };
+
 export default function () {
   // our HTTP request, note that we are saving the response to res, which can be accessed later
   const res = http.get('http://test.k6.io');
