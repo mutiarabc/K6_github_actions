98caf1e540853172546d36639391a5f39aef6dcd
tegarimansyah <egar.imansyah@gmail.com>
2021-04-28 15:40:14 +0700

    add k6 script teardown to github

--- README.md
+++ README.md
@@ -1,1 +1,146 @@
-# K6_github_actions
+# Github Actions for Performance Test Workflows
+
+Ada tiga tahap yang dilakukan ketika menjalankan performance test di Github Actions Workflow
+
+1. Deploy image system ke kubernetes (Deployment dan Service)
+2. Run k6 sebagai Job di Kubernetes
+3. Ketika selesai, lakukan teardown untuk semua instance yang telah dibuat
+
+Untuk itu, dibuatlah sebuah alur secara garis besar sebagai berikut:
+
+1. Manual trigger dari github 
+2. Pilih options seperti jenis test dan max VU
+3. Github actions menjalankan `performance_test.yml` (step 1 dan 2) dan menyelesaikan job nya (detach)
+4. Test selesai pada kubernetes, menjalankan fungsi teardown dimana melakukan POST Request ke Github Actions
+5. Github actions menjalankan `destroy_instance.yml` (step 3)
+
+## Manual Trigger dari Github
+
+To be added
+
+## Pilih Options
+
+* Load Test: 
+  * Ramp up **1/5**
+  * Hold **3/5**
+  * Ramp Down **1/5**
+* Soak Test: 
+  * Ramp up **1/8**
+  * Hold **6/8**
+  * Ramp Down **1/8**
+* Spike Test: 
+  * Distribusi Load **15%** lalu **100%**
+  * Ramp up Pertama **1/8**
+  * Hold Pertama **1/8**
+  * Ramp up Kedua **1/8**
+  * Hold Kedua **4/8**
+  * Ramp Down **1/8**
+* Stress Test: 
+  * Distribusi Load **50%** lalu **100%**
+  * Ramp up Pertama **1/5**
+  * Hold Pertama **1/5**
+  * Ramp up Kedua **1/5**
+  * Hold Kedua **1/5**
+  * Ramp Down **1/5**
+
+To be added
+
+## Performance Test Workflow
+
+To be Added
+
+
+## Teardown Function
+
+Teardown yang digunakan kurang lebih seperti ini. Dengan catatan secrets dan inputs dijadikan environment variable sehingga dapat diakses oleh K6.
+
+```js
+export function tearingdown() {
+
+    const url = basic_url + `/repos/${__ENV.USER_REPO}/${__ENV.REPO_NAME}/actions/workflows/${__ENV.WORKFLOW_FILE_NAME}/dispatches`;
+
+    const payload = JSON.stringify({
+      "ref": "main",
+      "inputs": {
+          "deployment_name": `${__ENV.SYSTEM_NAME}`,
+          "job_name": `${__ENV.JOB_NAME}`
+      }
+    });
+
+    const credentials = `${__ENV.USER_API}:${__ENV.GITHUB_TOKEN}`;
+    const encodedCredentials = encoding.b64encode(credentials);
+    console.log(encodedCredentials)
+    const params = {
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/vnd.github.v3+json',
+          'Authorization': `Basic ${encodedCredentials}`,
+      },
+    };
+
+    http.post(url, payload, params);  
+}
+```
+
+Kita menggunakan nama workflow file (termasuk ekstensinya) untuk melakukan trigger. Fungsi ini disimpan pada folder `misc` dengan nama `teardown.js` dan fungsi bernama `tearingdown` (perhatikan + `ing`). Pada script yang dijalankan, tambahkan fungsi sebagai berikut.
+
+```diff
+import http from "k6/http";
++ import { tearingdown } from './misc/teardown.js';
+
+...
+
++ export function teardown() {
++  tearingdown();
++ }
+```
+
+## Destroy Instance Workflow
+
+Perhatikan data yang dikirimkan pada tahap sebelumnya, yang menandakan kita memberi inputs berupa `deployment_name` dan `job_name`
+
+```json
+{
+  "ref": "main",
+  "inputs": {
+      "deployment_name": `${__ENV.SYSTEM_NAME}`,
+      "job_name": `${__ENV.JOB_NAME}`
+  }
+}
+```
+
+Untuk melakukan destroy instance, cukup melakukan `kubectl delete`. Contoh dibawah ini jika menggunakan step dari `steebchen/kubectl@master` digabungkan dengan data inputs yang diberikan pada proses sebelumnya.
+
+```yml
+name: Destroy Instance
+on: 
+  workflow_dispatch:
+    # Add KUBE_CONFIG_DATA in secrets
+    inputs:
+      # From previous step (POST request)
+      deployment_name: 
+        description: 'System Deployment Name'     
+        required: true
+      job_name:
+        description: 'K6 Job Name'
+        required: true
+
+jobs:
+  destroy:
+    runs-on: ubuntu-latest
+    steps:
+      # https://github.com/marketplace/actions/kubernetes-cli-kubectl
+      - name: Delete Deployment and Service
+        uses: steebchen/kubectl@master
+        env:
+          KUBE_CONFIG_DATA: ${{ secrets.KUBE_CONFIG_DATA }}
+        with:
+          args: delete deployments/${{github.event.inputs.deployment_name}} services/${{github.event.inputs.deployment_name}}-svc
+
+      - name: Delete Job
+        uses: steebchen/kubectl@master
+        env:
+          KUBE_CONFIG_DATA: ${{ secrets.KUBE_CONFIG_DATA }}
+        with:
+          args: delete job/${{github.event.inputs.job_name}}
+```
---
+++ main.js
@@ -0,0 +1,118 @@
+import { sleep, group } from "k6";
+import http from "k6/http";
+import { tearingdown } from './misc/teardown.js';
+
+export const options = {};
+
+export default function main() {
+  let response;
+
+  group("page_1 - http://test.k6.io/", function () {
+    response = http.get("http://test.k6.io/", {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        connection: "keep-alive",
+        cookie:
+          "_ga=GA1.2.632411891.1610344308; _hjid=ea3ce88e-4239-47e3-bc93-65375c91d7e9; k6_utm=%7B%22utm_campaign%22%3A%22postman%22%2C%20%22utm_source%22%3A%22blog%22%2C%22utm_medium%22%3A%22organic%22%7D; _lfa=LF1.1.d13facfdcb00913e.1619059718867; _gid=GA1.2.1041318581.1619418682; _gat=1",
+        host: "test.k6.io",
+        "upgrade-insecure-requests": "1",
+        "user-agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+      },
+    });
+    response = http.get("http://test.k6.io/static/css/site.css", {
+      headers: {
+        accept: "text/css,*/*;q=0.1",
+        "accept-encoding": "gzip, deflate",
+        "accept-language": "en-US,en;q=0.9",
+        connection: "keep-alive",
+        cookie:
+          "_ga=GA1.2.632411891.1610344308; _hjid=ea3ce88e-4239-47e3-bc93-65375c91d7e9; k6_utm=%7B%22utm_campaign%22%3A%22postman%22%2C%20%22utm_source%22%3A%22blog%22%2C%22utm_medium%22%3A%22organic%22%7D; _lfa=LF1.1.d13facfdcb00913e.1619059718867; _gid=GA1.2.1041318581.1619418682; _gat=1",
+        host: "test.k6.io",
+        referer: "http://test.k6.io/",
+        "user-agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+      },
+    });
+    response = http.get("http://test.k6.io/static/js/prisms.js", {
+      headers: {
+        accept: "*/*",
+        "accept-encoding": "gzip, deflate",
+        "accept-language": "en-US,en;q=0.9",
+        connection: "keep-alive",
+        cookie:
+          "_ga=GA1.2.632411891.1610344308; _hjid=ea3ce88e-4239-47e3-bc93-65375c91d7e9; k6_utm=%7B%22utm_campaign%22%3A%22postman%22%2C%20%22utm_source%22%3A%22blog%22%2C%22utm_medium%22%3A%22organic%22%7D; _lfa=LF1.1.d13facfdcb00913e.1619059718867; _gid=GA1.2.1041318581.1619418682; _gat=1",
+        host: "test.k6.io",
+        referer: "http://test.k6.io/",
+        "user-agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+      },
+    });
+    response = http.get("http://test.k6.io/static/favicon.ico", {
+      headers: {
+        accept:
+          "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+        "accept-encoding": "gzip, deflate",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        connection: "keep-alive",
+        cookie:
+          "_ga=GA1.2.632411891.1610344308; _hjid=ea3ce88e-4239-47e3-bc93-65375c91d7e9; k6_utm=%7B%22utm_campaign%22%3A%22postman%22%2C%20%22utm_source%22%3A%22blog%22%2C%22utm_medium%22%3A%22organic%22%7D; _lfa=LF1.1.d13facfdcb00913e.1619059718867; _gid=GA1.2.1041318581.1619418682; _gat=1",
+        host: "test.k6.io",
+        pragma: "no-cache",
+        referer: "http://test.k6.io/",
+        "user-agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+      },
+    });
+  });
+
+  group("page_2 - http://test.k6.io/flip_coin.php", function () {
+    response = http.get("http://test.k6.io/flip_coin.php", {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate",
+        "accept-language": "en-US,en;q=0.9",
+        connection: "keep-alive",
+        cookie:
+          "_ga=GA1.2.632411891.1610344308; _hjid=ea3ce88e-4239-47e3-bc93-65375c91d7e9; k6_utm=%7B%22utm_campaign%22%3A%22postman%22%2C%20%22utm_source%22%3A%22blog%22%2C%22utm_medium%22%3A%22organic%22%7D; _lfa=LF1.1.d13facfdcb00913e.1619059718867; _gid=GA1.2.1041318581.1619418682; _gat=1",
+        host: "test.k6.io",
+        referer: "http://test.k6.io/",
+        "upgrade-insecure-requests": "1",
+        "user-agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+      },
+    });
+  });
+
+  group("page_3 - http://test.k6.io/flip_coin.php?bet=heads", function () {
+    response = http.get("http://test.k6.io/flip_coin.php?bet=heads", {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate",
+        "accept-language": "en-US,en;q=0.9",
+        connection: "keep-alive",
+        cookie:
+          "_ga=GA1.2.632411891.1610344308; _hjid=ea3ce88e-4239-47e3-bc93-65375c91d7e9; k6_utm=%7B%22utm_campaign%22%3A%22postman%22%2C%20%22utm_source%22%3A%22blog%22%2C%22utm_medium%22%3A%22organic%22%7D; _lfa=LF1.1.d13facfdcb00913e.1619059718867; _gid=GA1.2.1041318581.1619418682; _gat=1",
+        host: "test.k6.io",
+        referer: "http://test.k6.io/flip_coin.php",
+        "upgrade-insecure-requests": "1",
+        "user-agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+      },
+    });
+  });
+
+  // Automatically added sleep
+  sleep(1);
+}
+
+export function teardown() {
+  tearingdown();
+}
